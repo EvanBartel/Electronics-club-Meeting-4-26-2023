dbbe8e6b4232c8f257f521ae25d02af764450363
EvanBartel <47606639+EvanBartel@users.noreply.github.com>
2023-04-26 13:14:14 -0400

    Create ESP32_Client.ino

---
+++ TwoESP32sTogether/ESP32_Client.ino
@@ -0,0 +1,24 @@
+#include <BLE_serial.h>
+
+BLE_serial SerialBT;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Trying to connect to ESP32_Server...");
+
+  if (!SerialBT.begin("ESP32_Server")) { // Set the target server device name
+    Serial.println("Couldn't find the target Bluetooth device!");
+    return;
+  }
+  
+  Serial.println("Connected to ESP32_Server!");
+}
+
+void loop() {
+  if (Serial.available()) {
+    SerialBT.write(Serial.read());
+  }
+  if (SerialBT.available()) {
+    Serial.write(SerialBT.read());
+  }
+}
